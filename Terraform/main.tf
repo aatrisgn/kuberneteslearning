aa117400b7363d1a89f9ae36d5034873633f267f
Asger Thyregod <asger.thyregod@gmail.com>
2024-09-11 20:08:49 +0200

    Changed subnet implementation

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -59,16 +59,21 @@ resource "azurerm_virtual_network" "secondary_vnet" {
   location            = azurerm_resource_group.secondary_rg.location
   resource_group_name = azurerm_resource_group.secondary_rg.name
   address_space       = ["10.11.0.0/16"]
+}
 
-  subnet {
-    name             = local.controller_subnet_name
-    address_prefixes = ["10.11.1.0/24"]
-  }
+resource "azurerm_subnet" "secondary_controller_subnet" {
+  name                 = local.controller_subnet_name
+  resource_group_name  = azurerm_resource_group.secondary_rg.name
+  virtual_network_name = azurerm_virtual_network.secondary_vnet.name
+  address_prefixes     = ["10.11.1.0/24"]
+}
 
-  subnet {
-    name             = local.worker_subnet_name
-    address_prefixes = ["10.11.2.0/24"]
-  }
+# Create subnet
+resource "azurerm_subnet" "secondary_worker_subnet" {
+  name                 = local.worker_subnet_name
+  resource_group_name  = azurerm_resource_group.secondary_rg.name
+  virtual_network_name = azurerm_virtual_network.secondary_vnet.name
+  address_prefixes     = ["10.11.2.0/24"]
 }
 
 resource "azurerm_virtual_network_peering" "peer_primary_to_secondary" {

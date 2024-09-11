89ef9fc35c5266de5d4a562c939366c271e5ced4
Asger Thyregod <asger.thyregod@gmail.com>
2024-09-11 20:29:19 +0200

    Updated image

--- Terraform/modules/linux_vm/main.tf
+++ Terraform/modules/linux_vm/main.tf
@@ -54,7 +54,7 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "24_04-lts-gen2"
+    sku       = "22_04-lts-gen2"
     version   = "latest"
   }
 
